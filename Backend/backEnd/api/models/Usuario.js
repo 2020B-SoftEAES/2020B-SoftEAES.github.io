956c64e0ef0788ecad8f839272ff782b636e80ee
William Suntaxi <69773296+WILLIAMSUNTAXI@users.noreply.github.com>
2021-03-15 21:54:10 -0500

    Creacion de los modelos y controladores DB

---
+++ Backend/backEnd/api/models/Usuario.js
@@ -0,0 +1,28 @@
+/**
+ * Usuario.js
+ *
+ * @description :: A model definition represents a database table/collection.
+ * @docs        :: https://sailsjs.com/docs/concepts/models-and-orm/models
+ */
+
+module.exports = {
+
+  tableName: 'db_persona',
+  attributes: {
+    nombre: {
+      type: 'string',
+      required: true,
+    },
+    apellido: {
+      type: 'string',
+      required: true
+    },
+    email: {
+      type: 'string',
+      required: true,
+      isEmail: true
+    }
+  },
+
+};
+
